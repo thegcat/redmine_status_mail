28175ee9f93e2d55ac3f2454d7d3227fa6545ae7
Felix Schäfer <schaefer@cypres-it.com>
2010-01-07 14:39:27 +0100

    somewhat ready patches?

--- init.rb
+++ init.rb
@@ -3,6 +3,6 @@ require 'redmine'
 Redmine::Plugin.register :redmine_status_mail do
   name 'Redmine Status Mail plugin'
   author 'Felix Schäfer'
-  description 'Adds a rake task to send users a single status email per day. This email will include stall (so many days since the start_date or since the last update) and ending-soon (so many days until end_date) tickets.'
+  description 'Adds a rake task to send users a single status email per day. This email will include stale (so many days since the start_date or since the last update) and ending-soon (so many days until end_date) tickets.'
   version 'trunk'
 end
